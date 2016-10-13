00036447270e0baa8d832ac568a8d9beee96c40c
avazquezsuarez <avazquezsuarez@victoria15.danielcastelao.org>
2016-10-13 08:52:21 +0200

    Propietario

---
+++ src/propietario_colaborador/Propietario_colaborador.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package propietario_colaborador;
+
+/**
+ *
+ * @author avazquezsuarez
+ */
+public class Propietario_colaborador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int prueba=2;
+        int hola= 1234;
+    }
+    
+}
